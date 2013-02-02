7092ba36d1c7ce2bd2f0a517eb126cd9bf9787cd
turner <csusbdt@gmail.com>
2013-02-02 05:50:55 -0800

    added bootstrap

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 app405
 ======
+
+The folder app405 is an Eclipse project that depends on the GAE plugin.
+
---
+++ app405/src/app/App405Servlet.java
@@ -0,0 +1,15 @@
+package app;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class App405Servlet extends HttpServlet
+{
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException
+	{
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
